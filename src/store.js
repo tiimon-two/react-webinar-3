764e63b098ddc795f82c6836521eaaa2dd2d7fe9
tiimon <aitama90p@gmail.com>
2023-11-22 10:09:55 +0400

    Доработал выделение записей, создание новой записи и добавил отображение счетчика кликов по элементу

--- src/store.js
+++ src/store.js
@@ -5,6 +5,7 @@ class Store {
   constructor(initState = {}) {
     this.state = initState;
     this.listeners = []; // Слушатели изменений состояния
+    this.number = this.state.list.length + 1;
   }
 
   /**
@@ -44,8 +45,9 @@ class Store {
   addItem() {
     this.setState({
       ...this.state,
-      list: [...this.state.list, {code: this.state.list.length + 1, title: 'Новая запись'}]
+      list: [...this.state.list, {code: this.number, title: 'Новая запись', selectCount : 0}]
     })
+    this.number++;
   };
 
   /**
@@ -69,6 +71,9 @@ class Store {
       list: this.state.list.map(item => {
         if (item.code === code) {
           item.selected = !item.selected;
+          item.selectCount++;
+        } else {
+          item.selected = false;
         }
         return item;
       })
